2090d7a46cf1f1075f53adda9b5c7f0aa20a608b
adahe8 <ada.he0908@gmail.com>
2023-03-03 11:48:38 -0800

    html cleanup

--- src/components/EventPosts.js
+++ src/components/EventPosts.js
@@ -3,21 +3,16 @@ import "./EventPosts.css";
 
 const EventPost = ({ post }) => {
     return(
-        <div className="event">
-            {
-                
-                [Object.values(post)].map((post) => {
-                    return <div className="card" key={post.at(0)}>
-                    <div className="event-container">
-                        <a href={'https://acmucsd.com/events/'+ post.at(5).replace(/ /g,'-').toLowerCase()+'-'+post.at(0)}><img src={post.at(4)} alt="" /></a>
-                        <h3><a href={'https://acmucsd.com/events/'+ post.at(5).replace(/ /g,'-').toLowerCase()+'-'+post.at(0)} target="_blank" rel="noreferrer">{post.at(5)}</a></h3>
-                        <p class="long">{post.at(6)}</p>
-                        <p>Time: {post.at(9)}</p>
-                    </div>
+        <div className="card" key={post.uuid}>
+            <div className="event-container">
+                <a href={'https://acmucsd.com/events/'+ post.title.replace(/ /g,'-').toLowerCase()+'-'+post.uuid}><img src={post.cover} alt="" /></a>
+                    <h3><a href={'https://acmucsd.com/events/'+ post.title.replace(/ /g,'-').toLowerCase()+'-'+post.uuid} target="_blank" rel="noreferrer">{post.title}</a></h3>
+                    <p className="long">{post.description}</p>
+                    {/* /*<p>Time: {Date.parse(post.start).toLocaleDateString()}</p>*/ }
                 </div>
-                })
-            }
-        </div>
+         </div>
+           
+        
     )         
     
 }
